f535cc7fe058d46ecbc075e92f23afd607cb5a52
Dany Shaanan <danyshaanan@gmail.com>
2021-06-29 15:44:08 +0300

    Rewrite coast_lines to py3

--- renderings/coast_lines/gen.py
+++ renderings/coast_lines/gen.py
@@ -4,41 +4,33 @@ import random, math
 rnd = random.Random()
 iterations = 12
 x, y = 450, 450
-r = min(x,y)/2 * 0.82 # initial shape radius is 82% of image radius
+r = min(x, y) / 2 * 0.82 # initial shape radius is 82% of image radius
 
-for gon in [2,3,4]:
-
-  points = [(x/2+r*math.cos(2.*math.pi*i/gon),y/2+r*math.sin(2.*math.pi*i/gon)) for i in range(gon)]
+for gon in [2, 3, 4]:
+  angles = [math.tau * i / gon for i in range(gon)]
+  points = [(x / 2 + r * math.cos(a), y / 2 + r * math.sin(a)) for a in angles]
 
   for seed in range(3):
     rnd.seed(seed)
-    lines = [(points[i],points[(i+1)%gon]) for i in range(gon)]
-    im = Image.new("L", (x,y), 255)
+    lines = [(points[i], points[(i + 1) % gon]) for i in range(gon)]
+    im = Image.new("L", (x, y), 255)
     draw = ImageDraw.Draw(im)
 
     for i in range(iterations):
-
-      for l in lines:
-        draw.line((l[0], l[1]), fill=175)
+      for l in lines: draw.line((l[0], l[1]), fill=175)
 
       newLines = []
-      for l in lines:
-        s = l[0]
-        e = l[1]
-        d = 0.32 * ((s[0]-e[0])**2 + (s[1]-e[1])**2)**0.5
-        alpha = rnd.random() * 2 * math.pi
-        mx = (s[0]+e[0])/2 + d * math.cos(alpha)
-        my = (s[1]+e[1])/2 + d * math.sin(alpha)
-        m = (mx, my)
-        newLines += [(s,m),(m,e)]
+      for s, e in lines:
+        d = 0.32 * math.hypot(s[0] - e[0], s[1] - e[1])
+        alpha = rnd.random() * math.tau
+        m = ((s[0] + e[0]) / 2 + d * math.cos(alpha), (s[1] + e[1]) / 2 + d * math.sin(alpha))
+        newLines += [(s, m), (m, e)]
       lines = newLines
 
-    for l in lines:
-      draw.line((l[0], l[1]), fill=0)
-    im.save('coast_line_' + str(gon) + '_' + str(seed) + '_0.png')
+    for l in lines: draw.line((l[0], l[1]), fill=0)
+    im.save(f'output/coast_line_{gon}_{seed}_0.png')
 
     im = Image.new("L", (x,y), 255)
-    draw = ImageDraw.Draw(im)
-    draw.polygon([l[0] for l in lines], fill=0)
-    im.save('coast_line_' + str(gon) + '_' +str(seed) + '_1.png')
+    ImageDraw.Draw(im).polygon([l[0] for l in lines], fill=0)
+    im.save(f'output/coast_line_{gon}_{seed}_1.png')
 
