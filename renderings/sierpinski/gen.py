de6e9722f1ab61f744a44e40fce1b87cc1efb87d
Dany Shaanan <danyshaanan@gmail.com>
2021-06-29 20:35:59 +0300

    Rewrite sierpinski to py3

--- renderings/sierpinski/gen.py
+++ renderings/sierpinski/gen.py
@@ -1,38 +1,30 @@
 from PIL import Image
 import random, math
 
-rnd = random.Random()
-size = 200
-points = 10000
+w, h = 200, 200
 
-
-for gon in range(3,7):
+for gon in range(3, 7):
     vertices = []
-    for a in [2*math.pi * i/gon for i in range(gon)]:
-        x = size/2 * (1 + math.cos(a - math.pi/2))
-        y = size/2 * (1 + math.sin(a - math.pi/2))
-        vertices += [(x, y)]
+    for a in [math.tau * i / gon for i in range(gon)]:
+        vertices += [(w / 2 * (1 + math.cos(a - math.tau / 4)), h / 2 * (1 + math.sin(a - math.tau / 4)))]
 
-    for iterations in [0,1,2,3,4,6,9]:
-        rnd.seed(0)
-        image = Image.new("L",(size,size),255)
+    for iterations in [0, 1, 2, 3, 4, 6, 9]:
+        rnd = random.Random(0)
+        rr, choice = rnd.randrange, rnd.choice
+        image = Image.new('1', (w, h), 1)
         pixel = image.load()
 
-        for p in range(points/3*gon):
-            x = rnd.randrange(size)
-            y = rnd.randrange(size)
+        for _ in range(3_000 * gon):
+            x, y = rr(image.width), rr(image.height)
 
-            if (size/2-x)**2+(size/2-y)**2<(size/2)**2:
+            if 4 * math.hypot(image.width / 2 - x, image.height / 2 - y) < image.width + image.height:
                 for _ in range (iterations):
-                    vertex = rnd.choice(vertices)
-                    p = (12-gon)/18.
-                    q = 1 - p
-                    x = x*p + vertex[0]*q
-                    y = y*p + vertex[1]*q
-                pixel[x,y] = 0
+                    p = (12 - gon) / 18
+                    v0, v1 = choice(vertices)
+                    x = x * p + v0 * (1 - p)
+                    y = y * p + v1 * (1 - p)
+                pixel[x, y] = 0
 
-        filename = 'sierpinski_' + str(gon) + '_' + str(iterations) + '.png'
-        image.save(filename)
-        print filename
+        image.save(f'output/sierpinski_{gon}_{iterations}.png')
 
 
