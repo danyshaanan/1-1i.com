f26400d6f6bdf1215cdc22b57eacecd00b71cd3d
Dany Shaanan <danyshaanan@gmail.com>
2021-06-29 20:55:33 +0300

    Rewrite lorenz_attractor to py3

--- renderings/lorenz_attractor/gen.py
+++ renderings/lorenz_attractor/gen.py
@@ -1,49 +1,23 @@
 #!/bin/python
 import PIL.Image, math
 
-p = 28
-o = 10
-b = 8/3.
-
-def iterate(x,y,z, step):
-  xChange = o * (y - x)
-  yChange = x * (p - z) - y
-  zChange = x * y - b * z
-  x += xChange * step
-  y += yChange * step
-  z += zChange * step
-  return (x,y,z)
-
-def project(x,y,z, teta):
-  horizontal = x*math.sin(teta) + z*math.cos(teta)
-  vertical = y
-  draw(horizontal, vertical)
-
-width = 800
-height = 400
-
-def draw(x,y):
-  size = 6
-  w = int(size*x+width/2) % width
-  h = int(size*y+height/2) % height
-  pixel[w,h] = 255
-
-
+step = 0.004
+p, o, b = 28, 10, 8/3
+zoom = 6
 frames = 66
 
 for i in range(frames):
-  image = PIL.Image.new("L",(width, height), 0)
+  image = PIL.Image.new('1', (800, 400), 0)
   pixel = image.load()
-  teta = 2.*math.pi*i/frames
+  teta = math.tau * i / frames
 
   x = y = z = 1
-  for _ in range(10000):
-    (x,y,z) = iterate(x,y,z, 0.004)
-    project(x,y,z, teta)
+  for _ in range(10_000):
+    x, y, z = x + (o * (y - x)) * step, y + (x * (p - z) - y) * step, z + (x * y - b * z) * step
+    px, py = x * math.sin(teta) + z * math.cos(teta), y
+    pixel[zoom * px + image.width / 2, zoom * py + image.height / 2] = 1
 
-  filename = "lorenz" + str(10000 + i) + ".png"
-  image.save(filename)
-  print filename
+  image.save(f'output/lorenz{10000 + i}.png')
 
 # the code above generates a set of still images,
 # which can be combined to an animated gif using this imagemagick command line:
